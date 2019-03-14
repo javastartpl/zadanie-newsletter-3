401b7bf2f95c3950e6417cf79002b64256451f9b
olektomek <olekstomek@gmail.com>
2019-03-15 00:04:46 +0100

    propozycja rozwiazania z próbą fixa java.lang.StackOverflowError

--- src/RangeMain.java
+++ src/RangeMain.java
@@ -2,11 +2,17 @@ public class RangeMain {
     public static void main(String[] args) throws InterruptedException {
         int randomNumberInRange = (int) (Math.random() * 101);
 
-        Number number = new Number(randomNumberInRange);
-        for (Integer next : number) {
-            System.out.println(next);
-            Thread.sleep(10);
-            main(new String[] {});
+        try {
+            Number number = new Number(randomNumberInRange);
+            for (Integer next : number) {
+                System.out.println(next);
+                Thread.sleep(10);
+                System.gc();
+                main(new String[]{});
+            }
+        } catch (java.lang.StackOverflowError so) {
+            System.gc();
+            so.printStackTrace();
         }
     }
 }
