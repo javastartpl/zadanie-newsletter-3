2aa7eab30b1a2786ca292eebe63c285a49da0ecb
Zdzisław Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2019-03-20 00:58:14 +0100

    Rozwiązanie zadania

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Range range=new Range(0,101);
+        for(Integer next: range){
+            System.out.println(next);
+        }
+    }
+}
