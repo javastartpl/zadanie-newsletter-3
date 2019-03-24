cd42c6d0cea15ab3c6bc9a0dec0e6951d8e03e96
Zdzisław Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2019-03-25 00:19:01 +0100

    code formatted

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-
-        Range range=new Range(0,101);
-        for(Integer next: range){
+        Range range = new Range(0, 101);
+        for (Integer next : range) {
             System.out.println(next);
         }
     }
--- src/Range.java
+++ src/Range.java
@@ -1,33 +1,15 @@
 import java.util.Iterator;
 import java.util.Random;
 
-public class Range implements Iterable<Integer>{
+public class Range implements Iterable<Integer> {
     private int startNumber;
     private int endNumber;
-
     public Range(int startNumber, int endNumber) {
         this.startNumber = startNumber;
         this.endNumber = endNumber;
     }
-
-    public int getStartNumber() {
-        return startNumber;
-    }
-
-    public void setStartNumber(int startNumber) {
-        this.startNumber = startNumber;
-    }
-
-    public int getEndNumber() {
-        return endNumber;
-    }
-
-    public void setEndNumber(int endNumber) {
-        this.endNumber = endNumber;
-    }
-
     @Override
-    public Iterator <Integer> iterator() {
-        return new Random().ints(startNumber,endNumber).iterator();
+    public Iterator<Integer> iterator() {
+        return new Random().ints(startNumber, endNumber).iterator();
     }
 }
